4aed4f1b4f6d0169cedbe4de57778101c70e0163
jjbrooks251 <jjbrooks251@outlook.com>
2019-06-04 15:38:33 +0100

    middle stage

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -1,11 +1,57 @@
 package com.qa.persistence.domain;
 
 public class Account {
-	
-	//This class needs to have:
-	//An id
-	//An Account Number
-	//A First Name
-	//A last Name
-	
+
+	// This class needs to have:
+	// An id
+	// An Account Number
+	// A First Name
+	// A last Name
+
+	private int id;
+	private int accountNo;
+	private String firstName;
+	private String lastName;
+
+	public Account(int id, int accountNo, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.accountNo = accountNo;
+		this.firstName = firstName;
+		this.lastName = lastName;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(int accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 }
--- src/main/java/com/qa/persistence/repository/AccountMapRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountMapRepository.java
@@ -5,37 +5,37 @@ import java.util.Map;
 
 import com.qa.persistence.domain.Account;
 
-public class AccountMapRepository implements AccountRepository{
-	
+public class AccountMapRepository implements AccountRepository {
+
 	Map<Integer, Account> accountMap = new HashMap<Integer, Account>();
-	
-	//You must provide concrete implementation for each of these methods
-	//do not change the method signature
-	//THINK - if the parameter is a String, or the return type is a String
-	//How can I convert to a String from an Object?
-	//What utility methods do I have available?
-	
-	//You must complete this section using TDD
-	//You can use the suggested tests or build your own.
+
+	// You must provide concrete implementation for each of these methods
+	// do not change the method signature
+	// THINK - if the parameter is a String, or the return type is a String
+	// How can I convert to a String from an Object?
+	// What utility methods do I have available?
+
+	// You must complete this section using TDD
+	// You can use the suggested tests or build your own.
 
 	public String getAllAccounts() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return "returned all accounts";
 	}
 
 	public String createAccount(String account) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return "Inserted new account";
 	}
 
-	public String deleteAccount(int accountNumber) {
-		// TODO Auto-generated method stub
-		return null;
+	public String deleteAccount(int accountNo) {
+
+		return "deleted chosen accounts";
 	}
 
-	public String updateAccount(int accountNumber, String account) {
-		// TODO Auto-generated method stub
-		return null;
+	public String updateAccount(int accountNo, String account) {
+
+		return "Updated chosen accounts";
 	}
 
 }
